9d8f22cffec36d40608748dd3571b7ee1665324b
OH_SeongHyeon <80639167+hso03@users.noreply.github.com>
2025-04-07 18:56:57 +0900

    Update IndexController.java
    12

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping(value = {"/index.html","/"})
-    public String index(){
+    public String indexa(){
         return "index/index";
     }
 }
