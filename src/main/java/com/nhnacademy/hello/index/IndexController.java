488cce08aa84aae193ae67f1d4da344692aa9e89
ATGN02-018 <ygw1026@naver.com>
2025-04-08 11:08:26 +0900

    login form 채택요망

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping(value = {"/index.html","/"})
-    public String indexa(){
-        return "index/index";
+//    @GetMapping(value = {"/index.html","/"})
+//    public String indexa(){
+//        return "index/index";
+//    }
+
+    @GetMapping(value = {"/", "/index.html"})
+    public String index() {
+        return "/index/index";
     }
 }
