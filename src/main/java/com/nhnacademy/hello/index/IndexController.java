7c29940f848652cef48dcaf279cb024efe865b28
nhnacademy <fhqht303@naver.com>
2025-04-08 20:51:48 +0900

    commit

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-//    @GetMapping(value = {"/index.html","/"})
-//    public String indexa(){
-//        return "index/index";
-//    }
+
+    @GetMapping(value = {"/test"})
+    public String indexa(){
+        return "index/index";
+    }
 
     @GetMapping(value = {"/", "/index.html"})
     public String index() {
