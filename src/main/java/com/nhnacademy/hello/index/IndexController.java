751cdeff56d5ddea7cb9e0d8c50efb5d144356c0
InSeong23 <seungin2360@gmail.com>
2025-04-09 14:36:06 +0900

    임의의 값을 작성된 대시보드

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-//    @GetMapping(value = {"/index.html","/"})
-//    public String indexa(){
-//        return "index/index";
-//    }
+
     @GetMapping(value = {"/", "/index.html"})
     public String index() {
         return "index/index";
