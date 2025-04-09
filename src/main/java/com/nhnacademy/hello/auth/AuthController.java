30352c92f6bfb516167d9bd2a23a074f245fc316
ATGN02-018 <ygw1026@naver.com>
2025-04-09 14:45:02 +0900

    jar target 경로 수정

--- src/main/java/com/nhnacademy/hello/auth/AuthController.java
+++ src/main/java/com/nhnacademy/hello/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.hello.auth;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
     @GetMapping("/login")
     public String loginPage() {
+        log.info("test");
         return "auth/login";
     }
 
