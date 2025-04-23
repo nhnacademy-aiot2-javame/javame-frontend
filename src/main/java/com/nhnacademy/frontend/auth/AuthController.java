e77bab0f4a8a2c7e6ba9bb5bd4a72352568baa33
jangwonsung <jangwonsung@gmail.com>
2025-04-23 13:33:34 +0900

    feat-javame-frontend-local #10 : 구조 설계

--- src/main/java/com/nhnacademy/frontend/auth/AuthController.java
+++ src/main/java/com/nhnacademy/frontend/auth/AuthController.java
@@ -3,8 +3,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
--- src/main/java/com/nhnacademy/frontend/dashboard/DashboardController.java
+++ src/main/java/com/nhnacademy/frontend/dashboard/DashboardController.java
@@ -2,8 +2,10 @@ package com.nhnacademy.frontend.dashboard;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1/environment")
 public class DashboardController {
 
     @GetMapping({"/dashboard", "/"})
