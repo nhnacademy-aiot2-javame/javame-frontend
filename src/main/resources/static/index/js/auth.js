2b07a613fddfd9e5a45b7da6e2c671fda8c5a427
OH_SeongHyeon <80639167+hso03@users.noreply.github.com>
2025-05-30 14:39:48 +0900

    Update auth.js

--- src/main/resources/static/index/js/auth.js
+++ src/main/resources/static/index/js/auth.js
@@ -40,7 +40,7 @@ export async function login(memberEmail, memberPassword) {
             }, 500);
         });
     } else {
-        const response = await fetch('http://gateway.javame.live/api/v1/auth/login', {
+        const response = await fetch('http://javame-gateway:10279/api/v1/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -122,7 +122,7 @@ export async function refreshAccessToken() {
         throw new Error('Refresh token is missing');
     }
 
-    const response = await fetch('http://localhost:10279/api/v1/auth/refresh', {
+    const response = await fetch('http://javame-gateway:10279/api/v1/auth/refresh', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
