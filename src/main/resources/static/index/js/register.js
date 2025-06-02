9c149abc29f13d2db2ac6ea245cba26294355106
OH_SeongHyeon <80639167+hso03@users.noreply.github.com>
2025-06-02 13:51:35 +0900

    Update register.js

--- src/main/resources/static/index/js/register.js
+++ src/main/resources/static/index/js/register.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = 'https://javame-gateway:10279/api/v1';
+const BASE_URL = 'https://gateway.javame.live/api/v1';
 async function register(memberData) {
     const response = await fetch(`${BASE_URL}/members/register`, {
         method: 'POST',
