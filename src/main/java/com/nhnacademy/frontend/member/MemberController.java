b40df3f6fc00264717e3e106780eefe45e9439a8
nhnacademy <fhqht303@naver.com>
2025-06-03 17:19:49 +0900

    feat-modify:  수정중

--- src/main/java/com/nhnacademy/frontend/member/MemberController.java
+++ src/main/java/com/nhnacademy/frontend/member/MemberController.java
@@ -8,15 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/members")
 public class MemberController {
 
-    @GetMapping("/member-list")
-    public String memberList() {
-        return "admin/members";
-    }
-
-    // 오너는 본인 회사 회원 리스트 페이지로 이동
-    @GetMapping("/member-list-owner")
-    public String memberListOwner() {
-        return "owner/member-list";  // templates/owner/member-list.html
+    @GetMapping("/mypage")
+    public String myPage() {
+        return "common/my-page";  // templates/common/my-page.html
     }
 
     // 회원 설정은 공통
@@ -25,13 +19,15 @@ public class MemberController {
         return "common/settings";  // templates/common/settings.html
     }
 
-    @GetMapping("/mypage")
-    public String myPage() {
-        return "common/my-page";  // templates/common/my-page.html
-    }
-
+    // 회원 권한 주기, owner, admin 가능
     @GetMapping("/permission")
     public String permissionPage() {
-        return "owner/permission";
+        return "owner/pendingmembers";
+    }
+
+    // 오너는 본인 회사 회원 리스트 페이지로 이동
+    @GetMapping("/member-list")
+    public String memberListOwner() {
+        return "owner/members";  // templates/owner/member-list.html
     }
 }
