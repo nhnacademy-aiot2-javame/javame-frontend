7f01b4a07ce869410250f1f97b4eabf2f3e56d44
OH_SeongHyeon <80639167+hso03@users.noreply.github.com>
2025-06-07 19:58:17 +0900

    auth.js localhost -> javame.live/api/v1으로 변경
    auth.js localhost -> javame.live/api/v1으로 변경

--- src/main/resources/static/index/js/auth.js
+++ src/main/resources/static/index/js/auth.js
@@ -5,7 +5,7 @@
 const TOKEN_KEY = 'accessToken';
 const REFRESH_KEY = 'refreshToken';
 const USE_MOCK_LOGIN = false;
-// const CICD_URL = 'https://javame.live/api/v1';
+const CICD_URL = 'https://javame.live/api/v1';
 
 
 window.logout = logout;
@@ -42,7 +42,7 @@ export async function login(memberEmail, memberPassword) {
             }, 500);
         });
     } else {
-        const response = await fetch('http://localhost:10279/api/v1/auth/login', {
+        const response = await fetch(CICD_URL + '/api/v1/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -123,7 +123,7 @@ export async function refreshAccessToken() {
         throw new Error('Refresh token is missing');
     }
 
-    const response = await fetch('https://javame.live/api/v1/auth/refresh', {
+    const response = await fetch(CICD_URL + '/api/v1/auth/refresh', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
