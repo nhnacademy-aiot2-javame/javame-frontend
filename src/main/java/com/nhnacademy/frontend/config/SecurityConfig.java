752b6e9d2a8b883f15498fc5a37e113a9ab827b3
saumonrose <wjdwldus0809@naver.com>
2025-06-10 01:58:25 +0900

    commit

--- src/main/java/com/nhnacademy/frontend/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/frontend/config/SecurityConfig.java
@@ -1,34 +1,34 @@
-package com.nhnacademy.frontend.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf().disable()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/css/**", "/js/**", "/images/**").permitAll()
-                        .anyRequest().permitAll() // 인증 안 걸고, 점진적 도입
-                )
-                .oauth2Login(oauth -> oauth
-                        .loginPage("/auth/login")
-                        .successHandler(oAuth2LoginSuccessHandler)
-                );
-        return http.build();
-    }
-}
+//package com.nhnacademy.frontend.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http
+//                .csrf().disable()
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/auth/**", "/css/**", "/js/**", "/images/**").permitAll()
+//                        .anyRequest().permitAll() // 인증 안 걸고, 점진적 도입
+//                )
+//                .oauth2Login(oauth -> oauth
+//                        .loginPage("/auth/login")
+//                        .successHandler(oAuth2LoginSuccessHandler)
+//                );
+//        return http.build();
+//    }
+//}
