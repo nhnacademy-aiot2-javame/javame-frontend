56f25639b99d26a98011c9e04a485e46a123c04a
Moon-janggon <suzung4@gmail.com>
2025-06-09 17:42:50 +0900

    feat: #3 Oauth 가입 로직 기본 구현

--- src/main/java/com/nhnacademy/frontend/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/frontend/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.nhnacademy.frontend.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
@@ -20,7 +27,7 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth -> oauth
                         .loginPage("/auth/login")
-                        .defaultSuccessUrl("/auth/oauth2/success", true) // 성공 시 redirect
+                        .successHandler(oAuth2LoginSuccessHandler)
                 );
         return http.build();
     }
